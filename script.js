b15834fe46a1f830cf278659e45b635f64e3a9b3
Panthaweekan_Somngam <tatanoobza1234@hotmail.com>
2022-08-06 19:39:43 +0700

    FINISH LAB5

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ const todoCtn = document.getElementById("todo-container");
 inputAdd.onkeyup = (event) => {
   if (event.key !== "Enter") return;
 
-  //your code here
+  if (inputAdd.value == "") {
+    alert("Todo cannot be empty");
+  } else {
+    addTodo(inputAdd.value, false);
+    inputAdd.value = "";
+    saveTodo();
+  }
 };
 
 function addTodo(title, completed) {
@@ -23,26 +29,62 @@ function addTodo(title, completed) {
   doneBtn.innerText = "Done";
   doneBtn.className = "btn btn-success me-2";
 
+  doneBtn.style.display = "none";
+
+  div.onmouseover = () => {
+    doneBtn.style.display = "";
+    deleteBtn.style.display = "";
+  };
+
+  div.onmouseout = () => {
+    doneBtn.style.display = "none";
+    deleteBtn.style.display = "none";
+  };
+
   //create delete button
   const deleteBtn = document.createElement("button");
   deleteBtn.innerText = "Delete";
   deleteBtn.className = "btn btn-danger";
 
+  deleteBtn.style.display = "none";
+
   //your code here
-  //append todo to HTML...
+  div.append(span);
+  span.append(doneBtn);
+  span.append(deleteBtn);
+  todoCtn.prepend(div);
+
   //define buttons event...
+  doneBtn.onclick = () => {
+    completed = !completed;
+    span.style.textDecoration = completed ? "line-through" : "";
+    saveTodo();
+  };
+
+  deleteBtn.onclick = () => {
+    todoCtn.removeChild(div);
+    saveTodo();
+  };
 }
 
 function saveTodo() {
   const data = [];
   for (const todoDiv of todoCtn.children) {
-    //your code here
+    const todoObject = {};
+    todoObject.title = todoDiv.children[0].innerText;
+    todoObject.completed =
+      todoDiv.children[0].style.textDecoration === "line-through";
+    data.push(todoObject);
   }
+  localStorage.setItem("Todo-list", JSON.stringify(data));
   //your code here
 }
 
 function loadTodo() {
-  //your code here
+  const data = JSON.parse(localStorsage.getItem("Todo-list"));
+  for (const todoObject of data.reverse()) {
+    addTodo(todoObject.title, todoObject.completed);
+  }
 }
 
 loadTodo();
